91cf2554f045d75e1a1f1d22fcde94362661b858
ethush <ethush@gmail.com>
2016-01-07 22:52:41 -0600

    Added paypal SDK and setUp config

--- app/src/main/java/com/example/ethush/digits_paypal_example/MainActivity.java
+++ app/src/main/java/com/example/ethush/digits_paypal_example/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ethush.digits_paypal_example;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.digits.sdk.android.AuthCallback;
@@ -10,8 +13,19 @@ import com.digits.sdk.android.Digits;
 import com.digits.sdk.android.DigitsAuthButton;
 import com.digits.sdk.android.DigitsException;
 import com.digits.sdk.android.DigitsSession;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,14 +35,18 @@ public class MainActivity extends AppCompatActivity {
     private static final String TWITTER_KEY = "46jzF45XvDoOpEkAAd33O9A6I";
     private static final String TWITTER_SECRET ="xuH7Y2mcrIj5uRg0nqyUJniI1B4GKTFxC9UJByTg8rDLzKk5Qq05025";
 
+    //Paypal set Up
+    private static PayPalConfiguration payPalConfiguration = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_NO_NETWORK)
+            .clientId("AZdodt3GYKvEkVvbxAXyFvCGbMW2dHVQZDIJJObQtVvXMwfaPbUOxhjaXxhHMDDwGGJWZEWG_Ko_lt7a");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new TwitterCore(authConfig), new Digits());
         setContentView(R.layout.activity_main);
-
-
 
 
         DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
@@ -48,5 +66,58 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+        Intent paypalIntent = new Intent(this, PayPalService.class);
+        paypalIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,payPalConfiguration);
+
+        startService(paypalIntent);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
+    public void onBuyPressed(View Pressed) {
+
+        //este es el pago
+        PayPalPayment payment = new PayPalPayment(new BigDecimal("1.75"), "USD", "sample item",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfiguration);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+            if (confirm != null) {
+                try {
+                    //Si el pago es exitoso parseamos el JSON devuelto por paypal
+                    Log.i("paymentExample", confirm.toJSONObject().toString(4));
+
+                    String response = confirm.toJSONObject().getJSONObject("response").getString("id");
+
+                    Log.i("Unique ID", response);
+
+                } catch (JSONException e) {
+                    Log.e("paymentExample", "Ha ocurrido un error: ", e);
+                }
+            }
+        }
+        else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.i("paymentExample", "Compra cancelada.");
+        }
+        else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+            Log.i("paymentExample", "Pago ó configuración invalida.");
+        }
     }
 }
