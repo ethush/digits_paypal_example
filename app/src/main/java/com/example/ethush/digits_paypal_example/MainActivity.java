c6ed41e1c75cafa45c922e80beff63151b000149
ethush <ethush@gmail.com>
2016-01-08 23:46:52 -0600

    Agregada funcionalidad de Conekta para generar cobros por tarjeta

--- app/src/main/java/com/example/ethush/digits_paypal_example/MainActivity.java
+++ app/src/main/java/com/example/ethush/digits_paypal_example/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.digits.sdk.android.AuthCallback;
@@ -13,11 +14,13 @@ import com.digits.sdk.android.Digits;
 import com.digits.sdk.android.DigitsAuthButton;
 import com.digits.sdk.android.DigitsException;
 import com.digits.sdk.android.DigitsSession;
+
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
 import com.paypal.android.sdk.payments.PaymentActivity;
 import com.paypal.android.sdk.payments.PaymentConfirmation;
+
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 
@@ -25,6 +28,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.math.BigDecimal;
+
+import io.conekta.conektasdk.Card;
+import io.conekta.conektasdk.Conekta;
+import io.conekta.conektasdk.Token;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -41,41 +48,101 @@ public class MainActivity extends AppCompatActivity {
             .clientId("AZdodt3GYKvEkVvbxAXyFvCGbMW2dHVQZDIJJObQtVvXMwfaPbUOxhjaXxhHMDDwGGJWZEWG_Ko_lt7a");
 
 
+    //Conekta basics
+    private Button btnTokenize;
+    private Card conektaCard;
+    private Token conektaToken;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //SetUp de las claves para auth de digits para numeros telefónicos
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new TwitterCore(authConfig), new Digits());
+
         setContentView(R.layout.activity_main);
 
+        //SetUp de Conekta para tokenizar tarjetas
+        Conekta.setPublicKey("key_Hor2Y2c5sUo8t8YgAG8erNQ");
+        Conekta.setApiVersion("0.3.0");
+        Conekta.collectDevice(this);
 
+        /*
+        * Sección de integración basica de digits login
+        * */
         DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
         digitsButton.setCallback(new AuthCallback() {
             @Override
             public void success(DigitsSession session, String phoneNumber) {
-                // TODO: associate the session userID with your user model
-                Toast.makeText(getApplicationContext(), "Authentication successful for "
+                //Se obtiene el numero de telefono y de la sesion si es un usuario valido con el hash para almacenarlo
+                Toast.makeText(getApplicationContext(), "Login exitoso para el numero: "
                         + phoneNumber, Toast.LENGTH_LONG).show();
-
             }
 
             @Override
             public void failure(DigitsException exception) {
-                Log.d("Digits", "Sign in with Digits failure", exception);
+                Log.d("Digits", "Algo ha salido mal al procesar la solicitud", exception);
             }
         });
 
 
 
+        /*
+        * Sección de integración basica con funcionalidades añadidas en las funciones
+        * -onDestroy
+        * -OnBuyPressed
+        * -onActivityResult
+        * */
         Intent paypalIntent = new Intent(this, PayPalService.class);
-        paypalIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,payPalConfiguration);
+        paypalIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfiguration);
 
         startService(paypalIntent);
+
+
+
+        /*
+        * Integración de Conekta SDK para generar tokens de compra vía tarjeta de cŕedito
+        * */
+        btnTokenize = (Button) findViewById(R.id.btnTokenize);
+
+        btnTokenize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Datos de ejemplo, el token es mostrado en un Toast y en el Log
+                conektaCard = new Card("Jhon Doe Tester","371449635398431","123","01","2016");
+                conektaToken = new Token(MainActivity.this);
+
+                //Crea el token para agregar el cargo
+                conektaToken.onCreateTokenListener(new Token.CreateToken() {
+                    @Override
+                    public void onCreateTokenReady(JSONObject data) {
+                        try {
+                            Log.d("CONEKTA INFO", data.getString("id"));
+
+                            Toast.makeText(MainActivity.this, data.getString("id"), Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Log.d("CONEKTA INFO", "Device fingerprint is :: " +Conekta.deviceFingerPrint(MainActivity.this));
+                    }
+                });
+                //Crea el token
+                conektaToken.create(conektaCard);
+
+            }
+        });
+
 
     }
 
     @Override
     protected void onDestroy() {
+        //al cerrar la ventana se detiene el activity de paypal si estuviera en proceso
         stopService(new Intent(this, PayPalService.class));
         super.onDestroy();
     }
@@ -86,10 +153,12 @@ public class MainActivity extends AppCompatActivity {
         PayPalPayment payment = new PayPalPayment(new BigDecimal("1.75"), "USD", "sample item",
                 PayPalPayment.PAYMENT_INTENT_SALE);
 
+        //Se  inicia el proceso y se le pasa el parametro de pago
         Intent intent = new Intent(this, PaymentActivity.class);
         intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfiguration);
         intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
 
+        //Se inicia y se queda a la espera de que la actividad de paypal termine
         startActivityForResult(intent, 0);
     }
 
@@ -97,27 +166,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        //Valida si la actividad iniciada por PayPal finalizó correctamente
         if (resultCode == Activity.RESULT_OK) {
             PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
             if (confirm != null) {
                 try {
                     //Si el pago es exitoso parseamos el JSON devuelto por paypal
-                    Log.i("paymentExample", confirm.toJSONObject().toString(4));
-
+                    Log.d("paymentExample", confirm.toJSONObject().toString(4));
+                    //Obtenemos el ID de pago y el UniqueID para referencias
                     String response = confirm.toJSONObject().getJSONObject("response").getString("id");
-
-                    Log.i("Unique ID", response);
+                    Log.d("Unique ID", response);
 
                 } catch (JSONException e) {
-                    Log.e("paymentExample", "Ha ocurrido un error: ", e);
+                    Log.d("paymentExample", "Ha ocurrido un error: ", e);
                 }
             }
         }
         else if (resultCode == Activity.RESULT_CANCELED) {
-            Log.i("paymentExample", "Compra cancelada.");
+            Log.d("paymentExample", "Compra cancelada.");
         }
         else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
-            Log.i("paymentExample", "Pago ó configuración invalida.");
+            Log.d("paymentExample", "Pago ó configuración invalida.");
         }
     }
 }
